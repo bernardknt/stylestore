73767a9ec470f7a549665db31059d88253122a0c
bernardknt <bernardnt@yahoo.co.uk>
2024-03-04 00:41:34 +0300

    6th Commit

---
+++ lib/model/products.dart
@@ -0,0 +1,41 @@
+class Product {
+  final String description;
+  final String image;
+  final String name;
+  final String storeId;
+  final double quantity;
+  final bool tracking;
+  final String id;
+  final double minimum;
+  final double amount;
+  final String barcode;
+
+  Product({
+    required this.description,
+    required this.image,
+    required this.name,
+    required this.storeId,
+    required this.quantity,
+    required this.tracking,
+    required this.id,
+    required this.minimum,
+    required this.amount,
+    required this.barcode,
+  });
+
+  // Factory constructor to create a Product from a map
+  factory Product.fromMap(Map<String, dynamic> map) {
+    return Product(
+      description: map['description'] ?? '',
+      image: map['image'] ?? '',
+      name: map['name'] ?? '',
+      storeId: map['storeId'] ?? '',
+      quantity: map['quantity']/1.0 ?? 0.0,
+      tracking: map['tracking'] ?? false,
+      id: map['id'] ?? '',
+      minimum: map['minimum'] /1.0?? 0.0,
+      amount: map['amount']/1.0 ?? 0.0,
+      barcode: map['barcode'] ?? '',
+    );
+  }
+}
